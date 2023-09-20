346b6d0b01ec9115016c64e37bd8efa0abe6a79a
jennifer mahaffey <jmahaffey1.prsvr@gmail.com>
2023-09-20 11:07:00 -0400

    pass

--- main.test.js
+++ main.test.js
@@ -6,7 +6,35 @@ let x = 3
 let y = 10
 
 let z = 4
+let greeting = 'hello';
+let firstName = 'Jennifer';
+let callStatus = 'ringing';
+ 
 
+function double()
+{
+  x = x * 2; 
+}
+
+function triple() {
+  y = y * 3;
+}
+
+function square()
+{
+ z = z * z;
+} 
+function beNice()
+{
+    greeting = 'hello' + firstName;}
+
+function changeName() {
+  firstName = 'Danger';
+
+}
+function hangUp() {
+  callStatus = 'call ended'
+}
 
 /*********************************************************************************************************************
 
